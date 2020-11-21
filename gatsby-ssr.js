0fdb0a6d290adc6e8efa114b66044b1529e87a75
Adam Azad <adam@adamazad.com>
2020-11-21 16:34:30 +0300

    Add root wrapper

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,2 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
-
-// You can delete this file if you're not using it
+import { wrapRootElement as wrapper } from './root-wrapper'
+export const wrapRootElement = wrapper
